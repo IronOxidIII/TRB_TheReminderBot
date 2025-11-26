2e2d22a6c0b0ca43b1c85ad63f842bd217817c9f
WallKing110 <ksalamahin2004@mail.ru>
2025-11-26 18:37:40 +0500

    Правки по ревью и подстройка под другие мр-ы.

--- thereminderbot/src/main/java/org/thereminderbot/components/App.java
+++ thereminderbot/src/main/java/org/thereminderbot/components/App.java
@@ -3,14 +3,15 @@ package org.thereminderbot.components;
 import org.thereminderbot.components.repository.RepositoryComponent;
 import org.thereminderbot.components.service.ServiceComponent;
 
+import java.io.PrintStream;
 import java.util.*;
 
 public class App {
     private final RepositoryComponent repositoryComponent;
     private final ServiceComponent serviceComponent;
-    private final Set<String> commands;
 
     private final Scanner scanner = new Scanner(System.in);
+    private final PrintStream userOut = System.out;
 
     /**
      * Конструктор по умолчанию.
@@ -18,8 +19,6 @@ public class App {
     public App() {
         repositoryComponent = new RepositoryComponent();
         serviceComponent = new ServiceComponent();
-        commands = new HashSet<>();
-        initializeCommands();
     }
 
     /**
@@ -29,10 +28,10 @@ public class App {
         printWelcome();
 
         while (true) {
-            System.out.print("Введите команду: ");
+            userOut.println("Введите команду: ");
             String command = scanner.nextLine().trim().toLowerCase();
             handleMenuInput(command);
-            System.out.println();
+            userOut.println();
         }
     }
 
@@ -110,29 +109,51 @@ public class App {
     }
 
     private void printWelcome() {
-        System.out.println("|----------------------------------------|");
-        System.out.println("|       Консольный TheReminderBot        |");
-        System.out.println("|----------------------------------------ad|");
-        printHelp();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("|----------------------------------------|");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("|       Консольный TheReminderBot        |");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("|----------------------------------------|");
+        stringBuilder.append(System.lineSeparator());
+
+        stringBuilder.append(getHelpMessage());
+        userOut.println(stringBuilder.toString());
     }
 
 
-    private void printHelp() {
-        System.out.println("Доступные команды:");
-        System.out.println("  /add_remind         - Добавить напоминание");
-        System.out.println("  /add_user           - Добавить пользователя");
-        System.out.println("  /delete_user        - Удалить пользователя");
-        System.out.println("  /delete_remind      - Удалить напоминание");
-        System.out.println("  /get_remind_info    - Информация о напоминании");
-        System.out.println("  /list_all_reminds   - Вывести все напоминания");
-        System.out.println("  /change_remind_text - Изменить текст напоминания");
-        System.out.println("  /change_username [username]   - Изменить имя пользователя");
-        System.out.println("  /help               - Эта справка");
+    private String getHelpMessage() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Справка по использованию:");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("Доступные команды:");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("  /add_remind                    - Добавить напоминание");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("  /add_user                      - Добавить пользователя");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("  /delete_user [userId]          - Удалить пользователя");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("  /delete_remind [userId]        - Удалить напоминание");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("  /get_remind_info [remindId]    - Информация о напоминании");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("  /list_all_reminds              - Вывести все напоминания");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("  /change_remind_text [remindId] - Изменить текст напоминания");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("  /change_username [userId]      - Изменить имя пользователя");
+        stringBuilder.append(System.lineSeparator());
+        stringBuilder.append("  /help                          - Эта справка");
+
+        return stringBuilder.toString();
     }
 
     private void handleMenuInput(String input) {
         if (input == null || input.isEmpty()) {
-            System.out.println("Ввод не можнт быть пустым.");
+            userOut.println("Ввод не можнт быть пустым.");
         }
 
         String[] inputArr = input.split(" ");
@@ -146,68 +167,68 @@ public class App {
                 break;
             case "/delete_user":
                 if (inputArr.length <= 1) {
-                    System.out.println(getMethodHelp(inputArr[0]));
+                    userOut.println(getMethodHelp(inputArr[0]));
                     break;
                 }
                 deleteUser(inputArr[1]);
                 break;
             case "/delete_remind":
                 if (inputArr.length <= 1) {
-                    System.out.println(getMethodHelp(inputArr[0]));
+                    userOut.println(getMethodHelp(inputArr[0]));
                     break;
                 }
                 delete_remind(inputArr[1]);
                 break;
             case "/get_remind_info":
                 if (inputArr.length <= 1) {
-                    System.out.println(getMethodHelp(inputArr[0]));
+                    userOut.println(getMethodHelp(inputArr[0]));
                     break;
                 }
                 getRemindInfo(inputArr[1]);
                 break;
             case "/get_user_info":
                 if (inputArr.length <= 1) {
-                    System.out.println(getMethodHelp(inputArr[0]));
+                    userOut.println(getMethodHelp(inputArr[0]));
                     break;
                 }
                 getUserInfo(inputArr[1]);
                 break;
             case "/help":
-                printHelp();
+                userOut.println(getHelpMessage());
                 break;
             case "/list_all_reminds":
                 listAllReminds();
                 break;
             case "/list_users_reminds":
                 if (inputArr.length <= 1) {
-                    System.out.println(getMethodHelp(inputArr[0]));
+                    userOut.println(getMethodHelp(inputArr[0]));
                     break;
                 }
                 listUsersReminds(inputArr[1]);
                 break;
             case "/change_remind_text":
                 if (inputArr.length <= 1) {
-                    System.out.println(getMethodHelp(inputArr[0]));
+                    userOut.println(getMethodHelp(inputArr[0]));
                     break;
                 }
                 changeRemindText(inputArr[1]);
                 break;
             case "/change_username":
                 if (inputArr.length <= 1) {
-                    System.out.println(getMethodHelp(inputArr[0]));
+                    userOut.println(getMethodHelp(inputArr[0]));
                     break;
                 }
                 changeUserName(inputArr[1]);
                 break;
             default:
-                System.out.println("Неизвестная команда. Введите /help");
+                userOut.println("Неизвестная команда. Введите /help");
                 break;
         }
     }
 
         private String getMethodHelp (String method) {
-        String prefix = "Использование команды: ";
-        return prefix + switch (method) {
+            String prefix = "Использование команды: ";
+            return prefix + switch (method) {
                 case "/delete_user" -> "/delete_user [userId] - Id пользователя";
                 case "/delete_remind" -> "/delete_remind [remindId] - Id напоминания";
                 case "/get_user_info" -> "/get_user_info [userId] - Id пользователя";
@@ -217,20 +238,6 @@ public class App {
                 case "/change_username" -> "/change_username [userId] - Id пользователя";
                 default -> "Нет справки для данного метода.";
             };
-        }
-
-        private void initializeCommands () {
-            commands.add("/add_remind");
-            commands.add("/add_user");
-            commands.add("/delete_user");
-            commands.add("/delete_remind");
-            commands.add("/get_remind_info");
-            commands.add("/get_user_info");
-            commands.add("/help");
-            commands.add("/list_all_reminds");
-            commands.add("/list_users_reminds");
-            commands.add("/change_remind_text");
-            commands.add("/change_username");
         }
 
     static public class ParsingHelper {
